57169898941134aa47e5ab6308c2601578c9b904
Androzelos <vejdetcubukcu@hotmail.com>
2024-12-06 22:16:15 +0300

    Added authentication middleware for login and logout routes

--- src/routes/users.mjs
+++ src/routes/users.mjs
@@ -2,6 +2,8 @@ import { response, Router } from "express";
 import { checkSchema } from "express-validator";
 import { createUserValidationSchema } from "../utils/validationSchema.mjs" 
 import { createUserHandler } from "../handlers/users.mjs";
+import { isAuthed } from "../utils/helpers.mjs";
+import { isNotAuthed } from "../utils/helpers.mjs";
 import passport from "passport";
 import "../strategies/local-strategy.mjs";
 
@@ -13,9 +15,22 @@ router.get('/api/users', (req,res) => {
 
 router.post('/api/users', checkSchema(createUserValidationSchema), createUserHandler);
 
-router.post('/api/users/login', passport.authenticate('local'), (req, res) => {
-    console.log(req.user);
+router.post('/api/users/login', isNotAuthed, passport.authenticate('local'), (req, res) => {
+    console.log(`User: ${req.user}`);
+    console.log(req.sessionID);
     return res.status(200).send({ msg: "succsesful" });
+})
+
+router.get('/api/users/logout', isAuthed, (req, res) => {
+    req.logout((err) => {
+        if (err) return res.sendStatus(400);
+        return res.status(200).send({ msg: "Logged Out" });
+    });
+})
+
+router.get('/api/users/me', (req, res) => {
+    console.log(req.sessionID);
+    return res.status(200).send(req.user);
 })
 
 export default router;
--- src/utils/helpers.mjs
+++ src/utils/helpers.mjs
@@ -10,3 +10,17 @@ export const hashPassword = (password) => {
 export const comparePassword = (plain, hashed) => {
     return bcrypt.compareSync(plain, hashed);
 }
+
+export const isAuthed = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.status(401).send('You have to be logged in to access this resource');
+}
+
+export const isNotAuthed = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        return next();
+    }
+    res.status(401).send('You have to be logged out to access this resource');
+}
