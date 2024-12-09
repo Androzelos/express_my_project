8340e0f8ca3bbcaff228bd70020cac26be4d9a43
Androzelos <vejdetcubukcu@hotmail.com>
2024-12-09 17:11:14 +0300

    Enhanced create team handler to validate team members and added error handling

--- src/handlers/teams.mjs
+++ src/handlers/teams.mjs
@@ -1,13 +1,21 @@
 import { Team } from '../mongoose/schema/teams.mjs';
 import { validationResult, matchedData } from 'express-validator'; 
+import { User } from '../mongoose/schema/user.mjs';
 
 export const createTeamHandler = async (req, res) => {
     const result = validationResult(req);
     if (!result.isEmpty()) return res.status(400).send(result.array());
 
     const data = matchedData(req);
-    console.log("Data:");
-    console.log(data);
+
+    const members = data.teamMembers.map(member => member.trim());
+
+    for (const member of members) {
+        const user = await User.findById(member);
+        if (!user) return res.status(400).send("User not found");
+        console.log(`Members id: ${member}`);
+    }
+
     const newTeam = new Team(data);
     try {
         console.log("Creating new team");
@@ -15,7 +23,11 @@ export const createTeamHandler = async (req, res) => {
         const savedTeam = await newTeam.save();
         return res.status(201).send(savedTeam);
     } catch (err) {
-        console.log(err.errorResponse);
-        return res.sendStatus(400);
+        console.log(err);
+        return res.status(400).send("An error occurred while creating a team");
     }
+}
+
+export const addUserToTeam = (req, res) => {
+    
 }
